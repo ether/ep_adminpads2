ether/ep_adminpads2

commit d5b8ae54014494d279e37c02b7a0358db4fc59c3

Replace double quotes with single quotes
This is a partial cherry-pick of:
https://github.com/RpNation/ep_adminpads3/commit/0340247959c9a77871f1e53794915ea880334c3a

index.js
@@ -1,13 +1,13 @@
-const eejs = require("ep_etherpad-lite/node/eejs");
-const padManager = require("ep_etherpad-lite/node/db/PadManager");
-const api = require("ep_etherpad-lite/node/db/API");
+const eejs = require('ep_etherpad-lite/node/eejs');
+const padManager = require('ep_etherpad-lite/node/db/PadManager');
+const api = require('ep_etherpad-lite/node/db/API');
 const queryLimit = 12;
 
 RegExp.quote = function (x) {
-    return x.toString().replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&")
+    return x.toString().replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')
 };
 const isNumeric = function (arg) {
-    return typeof (arg) == "number" || (typeof (arg) == "string" && parseInt(arg));
+    return typeof (arg) == 'number' || (typeof (arg) == 'string' && parseInt(arg));
 };
 
 let pads = {
@@ -19,19 +19,19 @@ let pads = {
     _do_search: async function (pads, query) {
         let data = {
                 progress: 1
-                , message: "Search done."
+                , message: 'Search done.'
                 , query: query
                 , total: pads.length
             }
             , maxResult = 0
             , result = []
         ;
-        if (query["pattern"] != null && query["pattern"] !== "") {
-            let pattern = "*" + query.pattern + "*";
+        if (query['pattern'] != null && query['pattern'] !== '') {
+            let pattern = '*' + query.pattern + '*';
             pattern = RegExp.quote(pattern);
-            pattern = pattern.replace(/(\\\*)+/g, ".*");
-            pattern = "^" + pattern + "$";
-            let regex = new RegExp(pattern, "i");
+            pattern = pattern.replace(/(\\\*)+/g, '.*');
+            pattern = '^' + pattern + '$';
+            let regex = new RegExp(pattern, 'i');
             pads.forEach(function (padID) {
                 if (regex.test(padID)) {
                     result.push(padID);
@@ -66,7 +66,7 @@ let pads = {
         data.results = [];
 
         rs.forEach(function (value) {
-            entrySet = {padName: value, lastEdited: "", userCount: 0};
+            entrySet = {padName: value, lastEdited: '', userCount: 0};
             data.results.push(entrySet);
         });
 
@@ -78,57 +78,57 @@ let pads = {
                 value.userCount = resultObject.padUsersCount;
             });
         } else {
-            data.message = "No results";
+            data.message = 'No results';
         }
         return data;
     }
 };
 
 exports.registerRoute = async function (hook_name, args) {
-    args.app.get("/admin/pads", function (req, res) {
+    args.app.get('/admin/pads', function (req, res) {
         let render_args = {
             errors: []
         };
-        res.send(eejs.require("ep_adminpads2/templates/admin/pads.html", render_args));
+        res.send(eejs.require('ep_adminpads2/templates/admin/pads.html', render_args));
     });
 };
 
 let io = null;
 
 exports.socketio = function (hook_name, args) {
-    io = args.io.of("/pluginfw/admin/pads");
-    io.on("connection", function (socket) {
-        socket.on("load", async function (query) {
-            let result = await pads.search({pattern: "", offset: 0, limit: queryLimit});
-            socket.emit("search-result", result);
+    io = args.io.of('/pluginfw/admin/pads');
+    io.on('connection', function (socket) {
+        socket.on('load', async function (query) {
+            let result = await pads.search({pattern: '', offset: 0, limit: queryLimit});
+            socket.emit('search-result', result);
         });
 
-        socket.on("search", async function (query) {
+        socket.on('search', async function (query) {
             let result = await pads.search(query);
-            socket.emit("search-result", result);
+            socket.emit('search-result', result);
         });
 
-        socket.on("delete", async function (padId) {
+        socket.on('delete', async function (padId) {
             let padExists = await padManager.doesPadExists(padId);
             if (padExists) {
                 //pad exists, remove
                 let pad = await padManager.getPad(padId);
                 await pad.remove();
-                socket.emit("progress", {progress: 1});
+                socket.emit('progress', {progress: 1});
             }
         });
     });
 };
 
 exports.updatePads = async function (hook_name, args) {
-    io.emit("progress", {progress: 1});
+    io.emit('progress', {progress: 1});
 };
 
 exports.eejsBlock_adminMenu = async function (hook_name, args) {
-    let hasAdminUrlPrefix = (args.content.indexOf("<a href=\"admin/") !== -1)
-        , hasOneDirDown = (args.content.indexOf("<a href=\"../") !== -1)
-        , hasTwoDirDown = (args.content.indexOf("<a href=\"../../") !== -1)
-        , urlPrefix = hasAdminUrlPrefix ? "admin/" : hasTwoDirDown ? "../../" : hasOneDirDown ? "../" : ""
+    let hasAdminUrlPrefix = (args.content.indexOf('<a href="admin"') !== -1)
+        , hasOneDirDown = (args.content.indexOf('<a href="../') !== -1)
+        , hasTwoDirDown = (args.content.indexOf('<a href="../../') !== -1)
+        , urlPrefix = hasAdminUrlPrefix ? 'admin/' : hasTwoDirDown ? '../../' : hasOneDirDown ? '../' : ''
     ;
-    args.content = args.content + "<li><a href=\"" + urlPrefix + "pads\">Manage pads</a></li>";
+    args.content = args.content + '<li><a href="' + urlPrefix + 'pads">Manage pads</a></li>';
 };

commit e9be53759f870d9247466ced206e0ddd7064a8cb

Fix typo introduced by d5b8ae54014494d279e37c02b7a0358db4fc59c3
I mistook `/"` as `\"` when I was converting quotes.

index.js
@@ -130,7 +130,7 @@ exports.updatePads = async function (hook_name, args) {
 };
 
 exports.eejsBlock_adminMenu = async function (hook_name, args) {
-    let hasAdminUrlPrefix = args.content.indexOf('<a href="admin"') !== -1,
+    let hasAdminUrlPrefix = args.content.indexOf('<a href="admin/') !== -1,
         hasOneDirDown = args.content.indexOf('<a href="../') !== -1,
         hasTwoDirDown = args.content.indexOf('<a href="../../') !== -1,
         urlPrefix = hasAdminUrlPrefix ? 'admin/' : hasTwoDirDown ? '../../' : hasOneDirDown ? '../' : '';