2dd966f2261e26984507bd85409aef81c56c4496
Richard Hansen <rhansen@rhansen.org>
2020-09-16 22:31:24 -0400

    Indent with 2 spaces instead of 4

--- index.js
+++ index.js
@@ -4,138 +4,138 @@ const api = require('ep_etherpad-lite/node/db/API');
 const queryLimit = 12;
 
 RegExp.quote = function (x) {
-    return x.toString().replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+  return x.toString().replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 const isNumeric = function (arg) {
-    return typeof arg == 'number' || (typeof arg == 'string' && parseInt(arg));
+  return typeof arg == 'number' || (typeof arg == 'string' && parseInt(arg));
 };
 
 let pads = {
-    pads: [],
-    search: async function (query) {
-        let the_pads = await padManager.listAllPads();
-        return await pads._do_search(the_pads.padIDs, query);
+  pads: [],
+  search: async function (query) {
+    let the_pads = await padManager.listAllPads();
+    return await pads._do_search(the_pads.padIDs, query);
+  },
+  _do_search: async function (pads, query) {
+    let data = {
+      progress: 1,
+      message: 'Search done.',
+      query: query,
+      total: pads.length,
     },
-    _do_search: async function (pads, query) {
-        let data = {
-            progress: 1,
-            message: 'Search done.',
-            query: query,
-            total: pads.length,
-        },
-            maxResult = 0,
-            result = [];
-        if (query['pattern'] != null && query['pattern'] !== '') {
-            let pattern = '*' + query.pattern + '*';
-            pattern = RegExp.quote(pattern);
-            pattern = pattern.replace(/(\\\*)+/g, '.*');
-            pattern = '^' + pattern + '$';
-            let regex = new RegExp(pattern, 'i');
-            pads.forEach(function (padID) {
-                if (regex.test(padID)) {
-                    result.push(padID);
-                }
-            });
-        } else {
-            result = pads;
+        maxResult = 0,
+        result = [];
+    if (query['pattern'] != null && query['pattern'] !== '') {
+      let pattern = '*' + query.pattern + '*';
+      pattern = RegExp.quote(pattern);
+      pattern = pattern.replace(/(\\\*)+/g, '.*');
+      pattern = '^' + pattern + '$';
+      let regex = new RegExp(pattern, 'i');
+      pads.forEach(function (padID) {
+        if (regex.test(padID)) {
+          result.push(padID);
         }
+      });
+    } else {
+      result = pads;
+    }
 
-        data.total = result.length;
+    data.total = result.length;
 
-        maxResult = result.length - 1;
-        if (maxResult < 0) {
-            maxResult = 0;
-        }
+    maxResult = result.length - 1;
+    if (maxResult < 0) {
+      maxResult = 0;
+    }
 
-        if (!isNumeric(query.offset) || query.offset < 0) {
-            query.offset = 0;
-        } else if (query.offset > maxResult) {
-            query.offset = maxResult;
-        }
+    if (!isNumeric(query.offset) || query.offset < 0) {
+      query.offset = 0;
+    } else if (query.offset > maxResult) {
+      query.offset = maxResult;
+    }
 
-        if (!isNumeric(query.limit) || query.limit < 0) {
-            query.limit = queryLimit;
-        }
+    if (!isNumeric(query.limit) || query.limit < 0) {
+      query.limit = queryLimit;
+    }
 
-        let rs = result.slice(query.offset, query.offset + query.limit);
+    let rs = result.slice(query.offset, query.offset + query.limit);
 
-        pads.pads = rs;
+    pads.pads = rs;
 
-        let entrySet;
-        data.results = [];
+    let entrySet;
+    data.results = [];
 
-        rs.forEach(function (value) {
-            entrySet = {padName: value, lastEdited: '', userCount: 0};
-            data.results.push(entrySet);
-        });
+    rs.forEach(function (value) {
+      entrySet = {padName: value, lastEdited: '', userCount: 0};
+      data.results.push(entrySet);
+    });
 
-        let getEdited = [];
-        if (data.results.length > 0) {
-            data.results.forEach(function (value) {
-                getEdited.push(
-                    api.getLastEdited(value.padName)
-                        .then((resultObject) => {
-                            value.lastEdited = resultObject.lastEdited;
-                            resultObject = api.padUsersCount(value.padName);
-                            value.userCount = resultObject.padUsersCount;
-                        }));
-            });
-        } else {
-            data.message = 'No results';
-        }
+    let getEdited = [];
+    if (data.results.length > 0) {
+      data.results.forEach(function (value) {
+        getEdited.push(
+          api.getLastEdited(value.padName)
+              .then((resultObject) => {
+                value.lastEdited = resultObject.lastEdited;
+                resultObject = api.padUsersCount(value.padName);
+                value.userCount = resultObject.padUsersCount;
+              }));
+      });
+    } else {
+      data.message = 'No results';
+    }
 
-        await Promise.all(getEdited);
-        return data;
-    },
+    await Promise.all(getEdited);
+    return data;
+  },
 };
 
 exports.expressCreateServer = function (hook_name, args, cb) {
-    args.app.get('/admin/pads', function (req, res) {
-        let render_args = {
-            errors: [],
-        };
-        res.send(eejs.require('ep_adminpads2/templates/admin/pads.html', render_args));
-    });
-    return cb();
+  args.app.get('/admin/pads', function (req, res) {
+    let render_args = {
+      errors: [],
+    };
+    res.send(eejs.require('ep_adminpads2/templates/admin/pads.html', render_args));
+  });
+  return cb();
 };
 
 let io = null;
 
 exports.socketio = function (hook_name, args) {
-    io = args.io.of('/pluginfw/admin/pads');
-    io.on('connection', function (socket) {
-        socket.on('load', async function (query) {
-            let result = await pads.search({pattern: '', offset: 0, limit: queryLimit});
-            socket.emit('search-result', result);
-        });
-
-        socket.on('search', async function (query) {
-            let result = await pads.search(query);
-            socket.emit('search-result', result);
-        });
-
-        socket.on('delete', async function (padId) {
-            let padExists = await padManager.doesPadExists(padId);
-            if (padExists) {
-                //pad exists, remove
-                let pad = await padManager.getPad(padId);
-                await pad.remove();
-                socket.emit('progress', {progress: 1});
-            }
-        });
+  io = args.io.of('/pluginfw/admin/pads');
+  io.on('connection', function (socket) {
+    socket.on('load', async function (query) {
+      let result = await pads.search({pattern: '', offset: 0, limit: queryLimit});
+      socket.emit('search-result', result);
     });
+
+    socket.on('search', async function (query) {
+      let result = await pads.search(query);
+      socket.emit('search-result', result);
+    });
+
+    socket.on('delete', async function (padId) {
+      let padExists = await padManager.doesPadExists(padId);
+      if (padExists) {
+        //pad exists, remove
+        let pad = await padManager.getPad(padId);
+        await pad.remove();
+        socket.emit('progress', {progress: 1});
+      }
+    });
+  });
 };
 
 exports.updatePads = function (hook_name, args, cb) {
-    io.emit('progress', {progress: 1});
-    return cb();
+  io.emit('progress', {progress: 1});
+  return cb();
 };
 
 exports.eejsBlock_adminMenu = function (hook_name, args, cb) {
-    let hasAdminUrlPrefix = args.content.indexOf('<a href="admin/') !== -1,
-        hasOneDirDown = args.content.indexOf('<a href="../') !== -1,
-        hasTwoDirDown = args.content.indexOf('<a href="../../') !== -1,
-        urlPrefix = hasAdminUrlPrefix ? 'admin/' : hasTwoDirDown ? '../../' : hasOneDirDown ? '../' : '';
-    args.content = args.content + '<li><a href="' + urlPrefix + 'pads" data-l10n-id="ep_adminpads2_manage-pads">Manage pads</a></li>';
-    return cb();
+  let hasAdminUrlPrefix = args.content.indexOf('<a href="admin/') !== -1,
+      hasOneDirDown = args.content.indexOf('<a href="../') !== -1,
+      hasTwoDirDown = args.content.indexOf('<a href="../../') !== -1,
+      urlPrefix = hasAdminUrlPrefix ? 'admin/' : hasTwoDirDown ? '../../' : hasOneDirDown ? '../' : '';
+  args.content = args.content + '<li><a href="' + urlPrefix + 'pads" data-l10n-id="ep_adminpads2_manage-pads">Manage pads</a></li>';
+  return cb();
 };
--- static/js/admin/pads.js
+++ static/js/admin/pads.js
@@ -1,188 +1,188 @@
 exports.documentReady = function(hooks, context, cb) {
-    if (context !== 'admin/pads') {
-        return cb;
+  if (context !== 'admin/pads') {
+    return cb;
+  }
+
+  var socket,
+      loc = document.location,
+      port = loc.port == '' ? (loc.protocol == 'https:' ? 443 : 80) : loc.port,
+      url = loc.protocol + '//' + loc.hostname + ':' + port + '/',
+      pathComponents = location.pathname.split('/'),
+      // Strip admin/plugins
+      baseURL = pathComponents.slice(0, pathComponents.length - 2).join('/') + '/',
+      resource = baseURL.substring(1) + 'socket.io';
+
+  var room = url + 'pluginfw/admin/pads';
+
+  var changeTimer;
+
+  //connect
+  socket = io.connect(room, {path: baseURL + 'socket.io', resource: resource});
+
+  $('.search-results').data('query', {
+    pattern: '',
+    offset: 0,
+    limit: 12,
+  });
+
+  var doUpdate = false;
+  var doAutoUpdate = function () {
+    return $('#results-autoupdate').prop('checked');
+  };
+
+  var search = function () {
+    clearTimeout(changeTimer);
+    socket.emit('search', $('.search-results').data('query'));
+  };
+
+  var htmlEntities = function (padName) {
+    return $('<div/>').text(padName).html();
+  };
+
+  var submitSearch = function () {
+    var query = $('.search-results').data('query');
+    query.pattern = $('#search-query')[0].value;
+    query.offset = 0;
+    search();
+  };
+
+  var isInt = function (input) {
+    return typeof input === 'number' && input % 1 === 0;
+  };
+
+  var formatDate = function (longtime) {
+    var formattedDate = '';
+    if (longtime != null && isInt(longtime)) {
+      var date = new Date(longtime);
+      var month = date.getMonth() + 1;
+      formattedDate = date.getFullYear() + '-' + fillZeros(month) + '-' + fillZeros(date.getDate()) + ' ' + fillZeros(date.getHours()) + ':' + fillZeros(date.getMinutes()) + ':' + fillZeros(date.getSeconds());
     }
+    return formattedDate;
+  };
 
-    var socket,
-        loc = document.location,
-        port = loc.port == '' ? (loc.protocol == 'https:' ? 443 : 80) : loc.port,
-        url = loc.protocol + '//' + loc.hostname + ':' + port + '/',
-        pathComponents = location.pathname.split('/'),
-        // Strip admin/plugins
-        baseURL = pathComponents.slice(0, pathComponents.length - 2).join('/') + '/',
-        resource = baseURL.substring(1) + 'socket.io';
+  var fillZeros = function (fillForm) {
+    return isInt(fillForm) ? (fillForm < 10 ? '0' + fillForm : fillForm) : '';
+  };
 
-    var room = url + 'pluginfw/admin/pads';
-
-    var changeTimer;
-
-    //connect
-    socket = io.connect(room, {path: baseURL + 'socket.io', resource: resource});
-
-    $('.search-results').data('query', {
-        pattern: '',
-        offset: 0,
-        limit: 12,
+  function updateHandlers() {
+    $('#progress.dialog .close').off('click').click(function () {
+      $('#progress.dialog').hide();
     });
 
-    var doUpdate = false;
-    var doAutoUpdate = function () {
-        return $('#results-autoupdate').prop('checked');
-    };
+    $('#search-form').off('submit').on('submit', function (e) {
+      e.preventDefault();
+      submitSearch();
+    });
 
-    var search = function () {
-        clearTimeout(changeTimer);
-        socket.emit('search', $('.search-results').data('query'));
-    };
+    $('#do-search').off('click').click(submitSearch);
 
-    var htmlEntities = function (padName) {
-        return $('<div/>').text(padName).html();
-    };
+    $('#search-query').off('change paste keyup').on('change paste keyup', function (e) {
+      clearTimeout(changeTimer);
+      changeTimer = setTimeout(function () {
+        e.preventDefault();
+        submitSearch();
+      }, 500);
+    });
 
-    var submitSearch = function () {
-        var query = $('.search-results').data('query');
-        query.pattern = $('#search-query')[0].value;
+    $('.do-delete').off('click').click(function (e) {
+      var row = $(e.target).closest('tr');
+      var padID = row.find('.padname').text();
+      if (confirm(_('ep_adminpads2_confirm', {padID: padID}) || `Do you really want to delete the pad ${padID}?`)) {
+        doUpdate = true;
+        socket.emit('delete', padID);
+      }
+    });
+
+    $('.do-prev-page').off('click').click(function (e) {
+      var query = $('.search-results').data('query');
+      query.offset -= query.limit;
+      if (query.offset < 0) {
         query.offset = 0;
-        search();
-    };
+      }
+      search();
+    });
+    $('.do-next-page').off('click').click(function (e) {
+      var query = $('.search-results').data('query');
+      var total = $('.search-results').data('total');
+      if (query.offset + query.limit < total) {
+        query.offset += query.limit;
+      }
+      search();
+    });
+  }
 
-    var isInt = function (input) {
-        return typeof input === 'number' && input % 1 === 0;
-    };
+  updateHandlers();
 
-    var formatDate = function (longtime) {
-        var formattedDate = '';
-        if (longtime != null && isInt(longtime)) {
-            var date = new Date(longtime);
-            var month = date.getMonth() + 1;
-            formattedDate = date.getFullYear() + '-' + fillZeros(month) + '-' + fillZeros(date.getDate()) + ' ' + fillZeros(date.getHours()) + ':' + fillZeros(date.getMinutes()) + ':' + fillZeros(date.getSeconds());
+  socket.on('progress', function (data) {
+    $('#progress .close').hide();
+    $('#progress').show();
+
+    $('#progress').data('progress', data.progress);
+
+    var message = _('ep_adminpads2_unknown-status') || 'Unknown status';
+    if (data.message) {
+      message = '<span class="status">' + data.message.toString() + '</span>';
+    }
+    if (data.error) {
+      message = '<span class="error">' + data.error.toString() + '<span>';
+    }
+    $('#progress .message').html(message);
+
+    if (data.progress >= 1) {
+      if (data.error) {
+        $('#progress').show();
+      } else {
+        if (doUpdate || doAutoUpdate()) {
+          doUpdate = false;
+          search();
         }
-        return formattedDate;
-    };
+        $('#progress').hide();
+      }
+    }
+  });
 
-    var fillZeros = function (fillForm) {
-        return isInt(fillForm) ? (fillForm < 10 ? '0' + fillForm : fillForm) : '';
-    };
+  socket.on('search-result', function (data) {
+    var widget = $('.search-results'),
+        limit = data.query.offset + data.query.limit;
+    if (limit > data.total) {
+      limit = data.total;
+    }
 
-    function updateHandlers() {
-        $('#progress.dialog .close').off('click').click(function () {
-            $('#progress.dialog').hide();
-        });
+    widget.data('query', data.query);
+    widget.data('total', data.total);
 
-        $('#search-form').off('submit').on('submit', function (e) {
-            e.preventDefault();
-            submitSearch();
-        });
+    widget.find('.offset').html(data.query.offset);
+    widget.find('.limit').html(limit);
+    widget.find('.total').html(data.total);
 
-        $('#do-search').off('click').click(submitSearch);
+    widget.find('.results *').remove();
+    var resultList = widget.find('.results');
 
-        $('#search-query').off('change paste keyup').on('change paste keyup', function (e) {
-            clearTimeout(changeTimer);
-            changeTimer = setTimeout(function () {
-                e.preventDefault();
-                submitSearch();
-            }, 500);
-        });
-
-        $('.do-delete').off('click').click(function (e) {
-            var row = $(e.target).closest('tr');
-            var padID = row.find('.padname').text();
-            if (confirm(_('ep_adminpads2_confirm', {padID: padID}) || `Do you really want to delete the pad ${padID}?`)) {
-                doUpdate = true;
-                socket.emit('delete', padID);
-            }
-        });
-
-        $('.do-prev-page').off('click').click(function (e) {
-            var query = $('.search-results').data('query');
-            query.offset -= query.limit;
-            if (query.offset < 0) {
-                query.offset = 0;
-            }
-            search();
-        });
-        $('.do-next-page').off('click').click(function (e) {
-            var query = $('.search-results').data('query');
-            var total = $('.search-results').data('total');
-            if (query.offset + query.limit < total) {
-                query.offset += query.limit;
-            }
-            search();
-        });
+    if (data.results.length > 0) {
+      data.results.forEach(function (resultset) {
+        var padName = resultset.padName;
+        var lastEdited = resultset.lastEdited;
+        var userCount = resultset.userCount;
+        var row = widget.find('.template tr').clone();
+        row.find('.padname').html('<a href="../p/' + encodeURIComponent(padName) + '">' + htmlEntities(padName) + '</a>');
+        row.find('.last-edited').html(formatDate(lastEdited));
+        row.find('.user-count').html(userCount);
+        resultList.append(row);
+      });
+    } else {
+      const noResults = _('ep_adminpads2_no-results') || 'No results';
+      resultList.append(
+          $('<tr>').append(
+              $('<td>')
+                  .attr('colspan', '4')
+                  .addClass('no - results')
+                  .text(noResults)));
     }
 
     updateHandlers();
+  });
 
-    socket.on('progress', function (data) {
-        $('#progress .close').hide();
-        $('#progress').show();
-
-        $('#progress').data('progress', data.progress);
-
-        var message = _('ep_adminpads2_unknown-status') || 'Unknown status';
-        if (data.message) {
-            message = '<span class="status">' + data.message.toString() + '</span>';
-        }
-        if (data.error) {
-            message = '<span class="error">' + data.error.toString() + '<span>';
-        }
-        $('#progress .message').html(message);
-
-        if (data.progress >= 1) {
-            if (data.error) {
-                $('#progress').show();
-            } else {
-                if (doUpdate || doAutoUpdate()) {
-                    doUpdate = false;
-                    search();
-                }
-                $('#progress').hide();
-            }
-        }
-    });
-
-    socket.on('search-result', function (data) {
-        var widget = $('.search-results'),
-            limit = data.query.offset + data.query.limit;
-        if (limit > data.total) {
-            limit = data.total;
-        }
-
-        widget.data('query', data.query);
-        widget.data('total', data.total);
-
-        widget.find('.offset').html(data.query.offset);
-        widget.find('.limit').html(limit);
-        widget.find('.total').html(data.total);
-
-        widget.find('.results *').remove();
-        var resultList = widget.find('.results');
-
-        if (data.results.length > 0) {
-            data.results.forEach(function (resultset) {
-                var padName = resultset.padName;
-                var lastEdited = resultset.lastEdited;
-                var userCount = resultset.userCount;
-                var row = widget.find('.template tr').clone();
-                row.find('.padname').html('<a href="../p/' + encodeURIComponent(padName) + '">' + htmlEntities(padName) + '</a>');
-                row.find('.last-edited').html(formatDate(lastEdited));
-                row.find('.user-count').html(userCount);
-                resultList.append(row);
-            });
-        } else {
-            const noResults = _('ep_adminpads2_no-results') || 'No results';
-            resultList.append(
-                $('<tr>').append(
-                    $('<td>')
-                        .attr('colspan', '4')
-                        .addClass('no - results')
-                        .text(noResults)));
-        }
-
-        updateHandlers();
-    });
-
-    socket.emit('load');
-    search();
-    return cb;
+  socket.emit('load');
+  search();
+  return cb;
 };
