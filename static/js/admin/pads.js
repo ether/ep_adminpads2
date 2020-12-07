945494f929c6494d704c89e39efc1cc00f46546e
HarryBleckert <HarryBleckert@users.noreply.github.com>
2020-12-07 14:43:08 +0800

    Add files via upload

--- static/js/admin/pads.js
+++ static/js/admin/pads.js
@@ -1,20 +1,30 @@
 /* global _ */
 'use strict';
 
+/*
+
+ToDos:
+fix AutoUpdate. It doesn't work, disabled in pads.html
+find better solution to 
+sort only single time and allow re-using result array
+
+*/
+
 $(() => {
   let query = {
     pattern: '',
     offset: 0,
-    limit: 12,
+    limit: 17000, //12
   };
   let total;
+  let queryFreq = 1000;   // 1 second wait for query update
+  const urlParams = new URLSearchParams(window.location.search);
 
   const basePath = location.pathname.split('/').slice(0, -2).join('/'); // Strip /admin/plugins.
   const socketioPath = `${basePath}/socket.io`;
   // Note: The socket.io URL should not contain ${basePath} because the path part of this URL is
   // used as the socket.io namespace.
   const socketioUrl = `${location.protocol}//${location.host}/pluginfw/admin/pads`;
-
   const socket = io.connect(socketioUrl, {path: socketioPath});
 
   let changeTimer;
@@ -33,10 +43,13 @@ $(() => {
     search();
   };
 
-  const formatDate = (longtime) => (new Date(longtime)).toLocaleString(undefined, {
-    dateStyle: 'short',
-    timeStyle: 'long',
-  });
+  const formatDate = (longtime) => {
+    //return (new Date(longtime)).toLocaleString(undefined, {
+    return (new Date(longtime)).toLocaleString("de-DE", {
+      dateStyle: 'short',
+      timeStyle: 'long',
+    });
+  };
 
   const updateHandlers = () => {
     $('#progress.dialog .close').off('click').click(() => $('#progress.dialog').hide());
@@ -47,13 +60,12 @@ $(() => {
     });
 
     $('#do-search').off('click').click(submitSearch);
-
-    $('#search-query').off('change paste keyup').on('change paste keyup', (e) => {
+    $('#search-query' ).off('change paste keyup').on('change paste keyup', (e) => {
       clearTimeout(changeTimer);
       changeTimer = setTimeout(() => {
         e.preventDefault();
         submitSearch();
-      }, 500);
+      }, queryFreq );
     });
 
     $('.do-delete').off('click').click((e) => {
@@ -122,22 +134,75 @@ $(() => {
     $('#offset').text(query.offset);
     $('#limit').text(limit);
     $('#total').text(total);
-
+    
     if (data.results.length > 0) {
       $('#loading').hide();
       $('#no-results').hide();
       $('#error').hide();
       const resultList = $('#results').empty();
+    
+    
+      var sortBy = "lastEdited";
+      var sortTags = ["padName", "lastEdited", "userCount", "padSize", "revisions"];
+      var sortParam = urlParams.get('sortby');
+      if ( sortParam && (sortTags.indexOf(sortParam) > -1) )
+      { sortBy = sortParam; }
+      var descending = true;
+      if ( Cookies.get("descending") === "false" )
+      {  descending = false; } 
+      if ( Cookies.get("sortBy") === sortBy && sortParam )
+      {  descending = !descending; }
+      else
+      {  descending = true; } //(sortBy == "padName" ?false :true); }
+      Cookies.set("sortBy", sortBy );
+      Cookies.set("descending", descending );
+      if ( sortBy == "padName" )
+      {  descending = !descending; }
+
+      data.results = data.results.slice(0);
+      if ( descending )
+      { data.results.sort(function(a,b) {
+        if ( sortBy == "padName" )
+        { return b.padName.localeCompare(a.padName); }
+        return b[sortBy] - a[sortBy];
+        });
+      }
+      else
+      { data.results.sort(function(a,b) {
+        if ( sortBy == "padName" )
+        { return a.padName.localeCompare(b.padName); }
+        return a[sortBy] - b[sortBy];
+        });
+      }
+      
+      var activeusers = 0;
+      var padSizeTotal = 0;
+      //sortedResults.forEach((resultset) => {
       data.results.forEach((resultset) => {
-        const {padName, lastEdited, userCount} = resultset;
+        const {padName, lastEdited, userCount, padSize, revisions} = resultset;
         const row = $('#template').clone().removeAttr('id');
-        row.find('.padname').empty().append(
-            $('<a>').attr('href', `../p/${encodeURIComponent(padName)}`).text(padName));
+        row.find('.padname').empty().append( $('<a>').attr('href', `../p/${encodeURIComponent(padName)}`).text(padName));
         row.find('.last-edited').text(formatDate(lastEdited));
+	row.find('.Size').text(padSize.toLocaleString("de-DE"));
+        row.find('.Revisions').text(revisions); //.toLocaleString("de-DE"));
         row.find('.user-count').text(userCount);
         resultList.append(row);
+        activeusers += userCount;
+	padSizeTotal += padSize;
       });
+      $('#activeusers').text(activeusers.toLocaleString("de-DE"));
+      $('#padsizetotal').text(padSizeTotal.toLocaleString("de-DE"));
+      //$('#results-autoupdate').prop('checked') = doAutoUpdate();
       $('#pad-widget').show();
+      //statistics
+      $('#totalPads').text(total); //text(data.Stats.totalPads);
+      //$('#totalSessions').text(data.Stats.totalSessions);
+      //$('#activePads').text(data.Stats.totalActivePads)
+
+      var sortCol = $('#'+sortBy).text();
+      var arrow   = descending ?" &darr;" :" &uarr;";
+      $('#'+sortBy).html( "<i>"+sortCol + "</i>" + arrow);      
+
     } else {
       $('#loading').hide();
       $('#pad-widget').hide();
